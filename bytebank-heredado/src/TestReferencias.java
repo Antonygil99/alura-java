d5380063b7c996cb792a5e8bff0172e22acddf9f
unknown <gilantonio656@gmail.com>
2023-03-19 19:06:07 -0600

    introduccion a polimorfismo

---
+++ bytebank-heredado/src/TestReferencias.java
@@ -0,0 +1,20 @@
+
+public class TestReferencias {
+	public static void main(String[] args) {
+		//El elemento mas generico puede ser adaptado al elemento mas
+		//especifico
+		//Funcionario funcionario = new Funcionario();
+		Funcionario funcionario = new Gerente();//pero no hereda los metodos del hijo 
+		funcionario.setNombre("Diego");
+		
+		Gerente gerente =  new Gerente();
+		gerente.setNombre("Ximena");
+		
+		funcionario.setSalario(2000);
+		gerente.setSalario(10000);
+		
+		
+		
+		
+	}
+}
