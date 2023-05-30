63f5a6fbaf04a15b1d1a6c65b6c5025fd240bddb
Antonygil99 <gilantonio656@gmail.com>
2023-05-30 12:49:56 -0600

    Metodos Adicionales sort de arraylist

--- data-collections/src/com/alura/ListasObjetos_MetodosAdicionales.java
+++ data-collections/src/com/alura/ListasObjetos_MetodosAdicionales.java
@@ -1,4 +1,5 @@
 package com.alura;
+
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
@@ -11,32 +12,39 @@ public class ListasObjetos_MetodosAdicionales {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		
+
 		Curso curso1 = new Curso("Ruby", 30);
 		Curso curso2 = new Curso("PHP", 10);
 		Curso curso3 = new Curso("JavaScript", 20);
 		Curso curso4 = new Curso("C", 50);
-		
+
 		ArrayList<Curso> cursos = new ArrayList<>();
 		cursos.add(curso1);
 		cursos.add(curso2);
 		cursos.add(curso3);
 		cursos.add(curso4);
-		
-		System.out.println(cursos);
-		
-		/*Para ser capaz de ordenar una lista de objetos es necesario 
-		implementar la interfaz colaborable que ya implementa el objeto Collections
-		e implementar los metodos de la interfaz en la clase correspondiente.
-		*/
-		Collections.sort(cursos);
-		System.out.println(cursos);
-		
-		Collections.sort(cursos, Collections.reverseOrder());
-		System.out.println(cursos);
-		
-		
 
+		System.out.println(cursos);
+
+		/*
+		 * Para ser capaz de ordenar una lista de objetos con la Clase Collections es
+		 * necesario implementar la interfaz colaborable que ya implementa el objeto
+		 * Collections e implementar los metodos de la interfaz en la clase
+		 * correspondiente.
+		 * 
+		 * Collections.sort(cursos); System.out.println(cursos);
+		 * 
+		 * Collections.sort(cursos, Collections.reverseOrder());
+		 * System.out.println(cursos);
+		 */
+		
+		/*
+		 * Para ser capaz de ordenar una lista de objetos con el metodo sort de la clase ArrayList
+		 * basta con añadir la siguiente linea de codigo:
+		 * Importante quitar la implementacion de la interfaz comparable<> en la clase curso
+		 */
+		cursos.sort(Comparator.comparing(Curso::getNombre));
+		System.out.println(cursos);
 		
 	}
 }
